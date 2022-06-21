08a9e707091d98e2f8c02d4bbc68c025a9e723d1
Camila Esteves <milasouza.cunha@gmail.com>
2022-06-21 17:53:48 -0300

    🔥 Removing lib from payment in Xamarin (#17)
    * 🔥 removing deep link lib
    
    * :pencil2: changing user name
    
    * :pencil2: deleting file

--- xamarin/PaymentDeeplinkDemo/MainActivity.cs
+++ xamarin/PaymentDeeplinkDemo/MainActivity.cs
@@ -1,12 +1,9 @@
 ﻿using Android.App;
 using Android.OS;
 using Android.Support.V7.App;
-using Android.Runtime;
 using Android.Widget;
-using System.Linq;
-using BR.Com.Stone.Posandroid.Paymentapp.Deeplink;
-using BR.Com.Stone.Posandroid.Paymentapp.Deeplink.Model;
-using BR.Com.Stone.Posandroid.Paymentapp.Deeplink.Exception;
+using System.Web;
+using Android.Content;
 using System;
 
 namespace PaymentDeeplinkDemo
@@ -20,44 +17,55 @@ namespace PaymentDeeplinkDemo
         )]
     public class MainActivity : AppCompatActivity
     {
-        PaymentDeeplink payment;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
-            payment = new PaymentDeeplink(this);
+
             Button button = FindViewById<Button>(Resource.Id.deepLinkButton);
             TextView resultTextView = FindViewById<TextView>(Resource.Id.deepLinkTextView);
+
+            Android.Net.Uri.Builder uriBuilder = new Android.Net.Uri.Builder();
+            uriBuilder.Authority("pay");
+            uriBuilder.Scheme("payment-app");
+
+            uriBuilder.AppendQueryParameter("amount", "200");
+            uriBuilder.AppendQueryParameter("editable_amount", "0"); // 1 = true and 0 = false
+            uriBuilder.AppendQueryParameter("transaction_type", "credit"); //DEBIT, CREDIT, VOUCHER
+            uriBuilder.AppendQueryParameter("installment_type", "merchant"); //MERCHANT, ISSUER, NONE
+            uriBuilder.AppendQueryParameter("return_scheme", "xamarindeeplink");
+            
+            String order_id = null;
+            if (order_id != null)
+            {
+                uriBuilder.AppendQueryParameter("order_id", order_id);
+            }
+
+            String installment_count = "2";
+            if (installment_count != null)
+            {
+                uriBuilder.AppendQueryParameter("installment_count", installment_count); // 2 to 99
+            }
+
             try
             {
-                PaymentResponse paymentResponse = payment.ReceiveDeeplinkResponse(Intent);
-                if (paymentResponse != null)
+                if (Intent.Data != null)
                 {
-                    resultTextView.Text = paymentResponse.ToString();
+                    resultTextView.Text = Intent.DataString;
                 }
-            }catch(PaymentException e)
+            }catch(Exception e)
             {
                 resultTextView.Text = e.Message;
             }
             
             button.Click += delegate
             {
-                SendDeepLink();
+                Intent intent = new Intent(Intent.ActionView);
+                intent.AddFlags(ActivityFlags.NewTask);
+                intent.SetData(uriBuilder.Build());
+                StartActivity(intent);
             };
-        }
-
-        private void SendDeepLink()
-        {
-            int amount = 100;
-            Java.Lang.Integer installmentCount = (Java.Lang.Integer) null;
-            String returnScheme = "xamarindeeplink";
-            Java.Lang.Integer orderId = null;
-            Java.Lang.Boolean editableAmount = Java.Lang.Boolean.False;
-
-            PaymentInfo paymentInfo = new PaymentInfo(amount, TransactionType.Credit, installmentCount, orderId, editableAmount, returnScheme, InstallmentType.Merchant);
-
-            payment.SendDeepLink(paymentInfo);
         }
     }
 }
